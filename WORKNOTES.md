7afce96e97f183517a88e189254841e00d4087a0
Daria <daria.kiseleva@mail.mcgill.ca>
2022-05-29 23:34:39 -0400

    Add Firebase config

---
+++ WORKNOTES.md
@@ -0,0 +1,2 @@
+- [ ] Come back to testing, did not delete those files
+- [ ] Not sure what to do with the index.css defaults, maybe delete later
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADuLHAsn5eYiLGHNm-17Z2sLhV03XVkNg",
+    authDomain: "ecommerce-81161.firebaseapp.com",
+    projectId: "ecommerce-81161",
+    storageBucket: "ecommerce-81161.appspot.com",
+    messagingSenderId: "595355744824",
+    appId: "1:595355744824:web:1376f282f1951d8ade84cd",
+    measurementId: "G-Q7Y82XTELL"
+  };
