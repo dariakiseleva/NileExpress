b20605c69363a7ea1640dde1391d86b533007202
Daria <daria.kiseleva@mail.mcgill.ca>
2022-06-03 01:26:01 -0400

    Display items with checkout with functionality to remove them

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -2,12 +2,31 @@ import React from 'react'
 import "./checkout.scss"
 
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from '../../StateProvider';
 
 export default function Checkout() {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <main className="checkout">
         <div className="checkout__left">
             <div className="checkout__title"><h2>Shopping cart</h2></div>
+            {/* Basket items */}
+
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+
+            ))}
+
+
         </div>
 
         <div className="checkout__right">
--- src/reducer.js
+++ src/reducer.js
@@ -4,11 +4,32 @@ export const initialState = {
 
 const reducer = (state, action) => {
     switch(action.type) {
-        case 'ADD_TO_BASKET':
+
+        //ACTION TO REMOVE FROM BASKET
+        case 'REMOVE_FROM_BASKET':
+            //Find the first item with the id of the item to delete
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
+            let newBasket = [...state.basket]
+            if (index >=0) {
+                //Chop off the item with that id
+                newBasket.splice(index, 1)
+            } else {
+                console.warn(`Can't remove product (id: ${action.id}) as it's not in the basket!`)
+            }
+
+            //Return with updated basket
             return {
                 ...state, 
+                basket: newBasket
+            };
+
+        //ACTION TO ADD TO BASKSET
+        case 'ADD_TO_BASKET':
+            return {
+                ...state,
                 basket: [... state.basket, action.item]
             };
+
         default: 
             return state;
     }
