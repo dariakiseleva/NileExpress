19cc7ff54c3e2e98ae16041af84557458a33fd58
Daria <daria.kiseleva@mail.mcgill.ca>
2022-06-03 02:53:46 -0400

    Set up handling login data on the front end in preparation for firebase

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,35 +1,70 @@
 import "./login.scss"
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 
 function Login() {
-  return (
-    <div className="login">
-        <Link to='/' style={{ textDecoration: 'none' }}>
-            <div className="login__logo">NileExpress</div>
-        </Link>
 
-        <div className="login__container">
-            <h1>Sign In</h1>
+    //States to store user input
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-            <form>
-                <h5>Email</h5>
-                <input type='text'/>
-                <h5>Password</h5>
-                <input type='password'/>
-                <button className="login__signInButton">Sign In</button>
-            </form>
+    //Handle login when button is clicked
+    const signIn = (e) => {
+        //Prevent prefresh on clicking button
+        e.preventDefault()
+        //Firebase login....
+    }
 
-            <p>Don't have an account?</p>
-            <button className="login__registerButton">Sign up</button>
+    //Handle registration when button is clicked
+    const register = (e) => {
+        e.preventDefault()
+        //Firebase register
+    }
+    
+
+    return (
+        <div className="login">
+            <Link to='/' style={{ textDecoration: 'none' }}>
+                <div className="login__logo">NileExpress</div>
+            </Link>
+
+            <div className="login__container">
+                <h1>Sign In</h1>
+
+                <form>
+                    <h5>Email</h5>
+                    <input 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        type='text'
+                    />
+                    <h5>Password</h5>
+                    <input 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        type='password'
+                    />
+                    <button 
+                        className="login__signInButton"
+                        onClick={signIn}
+                        type="submit"
+                    >Sign In</button>
+                </form>
+
+                <p>Don't have an account?</p>
+                <button 
+                    className="login__registerButton"
+                    onClick={register}
+                    type="submit"
+                >Sign up</button>
+
+            </div>
+
+
+
 
         </div>
-
-
-
-
-    </div>
-  )
+    )
 }
 
 export default Login
